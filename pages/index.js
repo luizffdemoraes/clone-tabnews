7138b0352e90b125edb2d1d9f1f0d053e66a3372
Luiz Felipe <lffm1994@gmail.com>
2024-03-20 03:13:20 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 function Home() {
-    return <h1>"Se eu vi mais longe, foi porque estava sobre os ombros de gigantes." Sir Isaac Newton</h1>
+  return (
+    <h1>
+      "Se eu vi mais longe, foi porque estava sobre os ombros de gigantes." Sir
+      Isaac Newton
+    </h1>
+  );
 }
 
 export default Home;
-
